fc088b0dc195209643aaacb734bafa0e53f2da61
hamBan <bsoham103@gmail.com>
2024-04-20 02:14:27 +0530

    Updated shard manager

--- Shard_Manager/shard_manager.py
+++ Shard_Manager/shard_manager.py
@@ -236,3 +236,7 @@ def start_health_check_thread():
     health_check_thread = threading.Thread(target=health_check)
     health_check_thread.daemon = True
     health_check_thread.start()
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 6000))
+    app.run(debug=True, host='0.0.0.0', port=port)
